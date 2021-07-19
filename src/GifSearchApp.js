a411e0c5b43128a0474b490d588f9bb7f54bb3b5
ismaeldiazy <ismaeldiazy@gmail.com>
2021-07-19 22:41:22 +0200

    removed default and unnecessary files and created GifSearchApp component

---
+++ src/GifSearchApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GifSearchApp = () => {
+    return (
+        <>
+            <h2>GifSearchApp</h2>
+            <hr />
+        </>
+    );
+}
+
+export default GifSearchApp;
