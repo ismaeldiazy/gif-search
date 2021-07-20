83ebeeb863431f64e02a80ffaaf5d45347030dda
ismaeldiazy <ismaeldiazy@gmail.com>
2021-07-20 21:24:27 +0200

    created form component

--- src/GifSearchApp.js
+++ src/GifSearchApp.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
 
 const GifSearchApp = () => {
+    // const categories = ['One punch', 'Samurai X', 'Dragon ball'];
+    const [categories, setCategories] = useState(['One punch', 'Samurai X', 'Dragon ball']);
+
+    // const handleAdd = () => {
+    //     // setCategories( ['Attack on Titan', ...categories]);
+    //     setCategories(cats => [...cats, 'Attack on Titan']);
+    // }
+
     return (
         <>
             <h2>GifSearchApp</h2>
+            <AddCategory />
             <hr />
+
+
+            <ol>
+                { 
+                    categories.map( category => {
+                        return <li key={ category }>{ category }</li>  
+                    })
+                }
+            </ol>
         </>
     );
 }
