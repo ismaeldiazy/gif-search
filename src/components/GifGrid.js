e534a6930b0550a928b289171f984f7adf5924d9
ismaeldiazy <ismaeldiazy@gmail.com>
2021-07-21 20:31:13 +0200

    Created GifGrid and GifGridItem components to render the obtained gifs via REST API. Created helper to contain the getGif function

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,29 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { GifGridItem } from './GifGridItem';
+import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
-    
-    const getGifs = async() => {
+    const apiKey = 'e4Rbc8Hq9DMMjYD6rbhsFUnRdqoxGorD';
+    const [images, setImages] = useState([]);
 
-        const url = 'https://api.giphy.com/v1/gifs/search?q=cheeseburger&limit=10&api_key=e4Rbc8Hq9DMMjYD6rbhsFUnRdqoxGorD'
-        const response = await fetch(url);
-        const { data } = await response.json();
+    useEffect( () => {
+        getGifs(category, 10, apiKey)
+            .then( setImages )
+    }, [category]);
 
-        const gifs = data.map( img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        });
+    // const getGifs = async() => {
 
-        console.log(gifs);
-    }
+    //     const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }=10&api_key=e4Rbc8Hq9DMMjYD6rbhsFUnRdqoxGorD`
+    //     const response = await fetch(url);
+    //     const { data } = await response.json();
 
-    getGifs();
+    //     const gifs = data.map( img => {
+    //         return {
+    //             id: img.id,
+    //             title: img.title,
+    //             url: img.images?.downsized_medium.url
+    //         }
+    //     });
+
+    //     // console.log(gifs);
+
+    //     // console.log(images);
+    //     setImages(gifs);
+    // }
+
 
     return (
-        <div>
-            <h3> { category } </h3>
+        <>
+        <h3> { category } </h3>
+        <div className="card-grid">
+            {
+                images.map( img => (
+                    <GifGridItem
+                        key={ img.id }
+                        { ...img }
+                        // key={ img.id } 
+                        // value={...img} 
+                    />
+                ))
+            }
         </div>
+        </>
     )
 }
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,22 @@
+
+
+
+ export const getGifs = async(category, limit, apiKey) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }=${limit}&api_key=${apiKey}`
+    const response = await fetch(url);
+    const { data } = await response.json();
+
+    const gifs = data.map( img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    });
+
+    // console.log(gifs);
+
+    // console.log(images);
+    return gifs;
+}
