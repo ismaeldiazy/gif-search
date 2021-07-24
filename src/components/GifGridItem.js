1715544a046a2117b94bbd7c63078d10839b65d6
ismaeldiazy <ismaeldiazy@gmail.com>
2021-07-24 18:04:38 +0200

    fix && added functionality to copy gif url to the clipboard by clicking the gif

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -3,8 +3,7 @@ import React from 'react'
 export const GifGridItem = ( {title, url} ) => {
     return (
         <div className="card animate__animated animate__fadeIn">
-            <img src={ url } alt={ title } />
-            <p>{ title }</p>
+            <img src={ url } alt={ title } onClick={ () => {navigator.clipboard.writeText(url)} }/>
         </div>
     )
 }
